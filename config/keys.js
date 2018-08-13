1f994fc3b9e8932ff84c7732c005f30774469421
Tony Carbetta <ruquss@gmail.com>
2018-08-12 19:22:38 -0700

    user registration to database with postman/post request

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb://CarbettaCoding:Kingston2012@ds035846.mlab.com:35846/devconnector"
+};
+
+//see authorized user for credentials
+//<username>:<password>
